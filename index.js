6d7fe8c10fef5417b9563b39b2ee6801a0b54174
Harvey <taipham.it@gmail.com>
2020-04-03 15:00:25 +0000

    update version

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var app = express();
 app.get('/', function (req, res) {
  res.send('hello world');
 });
+app.get('/version', function (req, res) {
+ res.send('v0.0.2');
+});
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
- 
 module.exports = app;
